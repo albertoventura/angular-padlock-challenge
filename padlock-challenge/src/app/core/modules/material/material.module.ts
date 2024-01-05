fa18b3e299a8f72f784f3f6b0ae3d9770bf060d3
Alberto Ventura <albertoventura.dev@gmail.com>
2024-01-05 15:16:49 -0300

    material works

---
+++ padlock-challenge/src/app/core/modules/material/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+const materialModules = [
+  MatProgressSpinnerModule,
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...materialModules,
+  ],
+  exports: [
+    ...materialModules,
+  ]
+})
+export class MaterialModule { }
